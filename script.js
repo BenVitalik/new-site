ebec82c13798d3f9242ffb289ef088f382359074
V1talik.Ben <magaflurry@gmail.com>
2026-02-17 16:11:01 +0100

    Створення нового сайті і перший коміт

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeImage(element) {
+    document.getElementById("mainImage").src = element.src;
+}
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+
+openBtn.onclick = () => modal.style.display = "flex";
+closeBtn.onclick = () => modal.style.display = "none";
+
+window.onclick = (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+};
